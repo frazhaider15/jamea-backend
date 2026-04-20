1ae4fd8a04cf8464ed7ca7e288ee49f7981d675d
Faraz Haider <frazhaider15@gmail.com>
2026-04-20 18:27:01 +0500

    fixed migration issue

--- models/masool_report.go
+++ models/masool_report.go
@@ -4,6 +4,6 @@ type MasoolReport struct {
 	ID       uint         `gorm:"primaryKey" json:"id"`
 	MasoolID uint         `gorm:"not null;index" json:"masool_id"`
 	Module   Module       `gorm:"not null;index" json:"module"`
-	Month    string       `gorm:"not null;index" json:"month"`
+	Month    string       `gorm:"not null;index;default:''" json:"month"`
 	Data     []MasoolData `gorm:"type:jsonb;serializer:json" json:"data"`
 }
